fa6735f042d29137a38450701832d2d57fb41170
Chris Spohn <chris.j.spohn@gmail.com>
2021-04-10 11:27:33 -0600

    Add simple route structure
    Co-authored-by: Elsa Fluss <elsafluss@users.noreply.github.com>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,40 +1,30 @@
-import Chessboard from 'chessboardjsx';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-
-interface BoardDetails {
-  fen: string;
-  draggable?: boolean;
-  width?: number;
-}
+import Splash from '../Splash/Splash'
 
 const App: React.FC = () => {
-  const currentBoard: BoardDetails = {
-    fen: "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
-    draggable: false,
-    width: 500,
-  }
-
   return ( 
-    <div className="App">
-      <header className="App-header">
-        <Chessboard
-          position={currentBoard.fen} 
-          draggable={currentBoard.draggable}
-          width={currentBoard.width}
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route 
+          exact
+          path="/"
+          component={Splash}
+        ></Route>
+        <Route
+          exact
+          path="/dashboard/:user"
+          render={({match}) => {
+            return <p>`Welcome ${match.params.user}`</p>
+          }}
+        ></Route>
+        <Route render={() => {
+          return <p>404</p>
+        }}
+        ></Route>
+
+      </Switch>
+    </>
   );
 }
 
--- src/components/Splash/Splash.tsx
+++ src/components/Splash/Splash.tsx
@@ -1,1 +1,13 @@
-export default null
+import React from 'react'
+
+const Splash = () => {
+  return (
+    <>
+      <h1>Welcome to Chess Quest</h1>
+    </>
+  )
+}
+
+
+
+export default Splash;
