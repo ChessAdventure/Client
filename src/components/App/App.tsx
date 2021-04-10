660063cd05312295f79ed970d2a09f53a0fe1494
Chris Spohn <chris.j.spohn@gmail.com>
2021-04-10 11:01:25 -0600

    Install router
    Co-authored-by: Elsa Fluss <elsafluss@users.noreply.github.com>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,7 +7,7 @@ interface BoardDetails {
   width?: number;
 }
 
-function App() {
+const App: React.FC = () => {
   const currentBoard: BoardDetails = {
     fen: "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
     draggable: false,
