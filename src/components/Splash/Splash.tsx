7bc1aec066e4b6f2023058388588f4299497da19
Chris Spohn <chris.j.spohn@gmail.com>
2021-04-10 19:54:19 -0600

    Add styling for splash page

--- src/components/Splash/Splash.tsx
+++ src/components/Splash/Splash.tsx
@@ -1,13 +1,21 @@
 import React, {MouseEvent} from 'react'
 import {Link} from 'react-router-dom'
-
+import './Splash.css'
+import chessPieces from '../../chess.svg'
 const Splash = () => {
 
   return (
-    <>
-      <h1>Welcome to Chess Quest</h1>
-      <Link to={'/dashboard/1234'}>Enter Site</Link>
-    </>
+    <div className="splash">
+      <div className="checkerboard">
+      </div>
+      <section className="login-modal">
+        <h1 className="title">Chess Quest</h1>
+        <div className="enter">
+          <Link to={'/dashboard/1234'}>BEGIN</Link>
+        </div>
+      </section>
+      <img className="chess-pieces" src={chessPieces} alt="chess pieces" />
+    </div>
   )
 }
 
