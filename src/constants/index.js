ee7a37305a07f38fffba83ce294a92f57946194f
Chris Spohn <chris.j.spohn@gmail.com>
2021-04-16 15:22:09 -0600

    Add s

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_ROOT = "https://chess-adventure-backend.herokuapp.com"
-export const API_WS_ROOT = "ws://chess-adventure-backend.herokuapp.com/cable"
+export const API_WS_ROOT = "wss://chess-adventure-backend.herokuapp.com/cable"
 export const HEADERS = {
   "Content-Type": "application/json",
   Accept: "application/json",
