924fdf4a5e0962a883f64112cbae15a1c8b3b05c
Elsa Fluss <elsafluss@gmail.com>
2021-04-17 16:17:28 -0600

    Style login page for medium screens

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { API_ROOT } from '../../constants/index'
-
 import './SignUp.css'
 
 interface PropTypes {
