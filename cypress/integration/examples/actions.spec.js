120b71be8bf085a8273a6bdb83949adbabb980e4
Elsa Fluss <elsafluss@gmail.com>
2021-04-19 11:42:32 -0600

    Write Cypress test to log in a new user

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -51,19 +51,14 @@ context("Actions", () => {
     cy.get(".log-in-wrapper")
       .should("exist")
       .should("have.descendants", "button")
-    
-    cy.get(".log-in")
-      .should("exist")
-      .should("have.text", "Enter")
 
-    cy.get(".signup-button")
-      .should("exist")
-      .should("have.text", "Sign Up")
+    cy.get(".log-in").should("exist").should("have.text", "Enter")
+
+    cy.get(".signup-button").should("exist").should("have.text", "Sign Up")
   })
 
   it("should display the sign up screen", () => {
-    cy.get(".signup-button")
-      .click()
+    cy.get(".signup-button").click()
 
     cy.get(".splash").should("exist").should("have.descendants", "section")
 
@@ -105,6 +100,19 @@ context("Actions", () => {
       .should("have.attr", "type", "password")
       .should("have.attr", "class", "input")
       .should("have.attr", "name", "password")
+    cy.get(".label")
+      .eq(1)
+      .should("exist")
+      .should("have.text", "Password:")
+      .should("have.descendants", "br")
+      .should("have.descendants", "input")
+
+    cy.get("input")
+      .eq(2)
+      .should("exist")
+      .should("have.attr", "type", "password")
+      .should("have.attr", "class", "input")
+      .should("have.attr", "name", "confirmPassword")
 
     cy.get(".log-in-wrapper")
       .should("exist")
@@ -113,5 +121,34 @@ context("Actions", () => {
     cy.get(".log-in").should("exist").should("have.text", "Enter")
 
     cy.get(".signup-button").should("exist").should("have.text", "Log In")
+  })
+
+  it.skip("should sign up the user", () => {
+    cy.get(".signup-button").click()
+
+    cy.get("input").eq(0).type("test user name5")
+
+    cy.get("input").eq(1).type("testpassword")
+    cy.get("input").eq(2).type("testpassword")
+
+    cy.get(".log-in").click({ force: true })
+
+    cy.intercept(
+      {
+        method: "POST",
+        url: "http://localhost:3001/api/v1/login",
+      },
+      {
+        statusCode: 200,
+        body: "../fixtures/login.json",
+      }
+    )
+
+    cy.location().should((loc) => {
+      expect(loc.host).to.eq("localhost:3000")
+      expect(loc.href).to.eq("http://localhost:3000/dashboard")
+
+    })
+      .should("exist")
   })
 })
