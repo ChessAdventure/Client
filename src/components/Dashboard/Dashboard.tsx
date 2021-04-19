733f95a0efbc4f67d9f130a5dc248146efc0d0f3
Elsa Fluss <elsafluss@gmail.com>
2021-04-19 14:06:09 -0600

    Adjust dashboard styling, including header button

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,9 @@
-import React, { Dispatch, SetStateAction} from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
 import Header from '../Header/Header'
 import QuestStart from '../QuestStart/QuestStart'
-import Thumbnail from '../UIComponents/Thumbnail/Thumbnail'
-import PastGames from '../PastGames/PastGames'
-
-import './Dashboard.css'
 import OldGameBoard from '../OldGameBoard/OldGameBoard'
+import Thumbnail from '../UIComponents/Thumbnail/Thumbnail'
+import './Dashboard.css'
 
 interface PropTypes {
   user: string;
@@ -15,18 +13,20 @@ interface PropTypes {
 
 const Dashboard = ({ user, setGameId, userKey }: PropTypes) => {
   return (
-  <>
+    <>
       <Header />
       <section className="container">
-        <QuestStart setGameId={setGameId} userKey={userKey}/>
-        <Thumbnail 
+        <br></br>
+        <Thumbnail
           imageSource="https://cdn11.bigcommerce.com/s-9nmdjwb5ub/images/stencil/1280x1280/products/153/1145/Business_Shark_big__95283.1513045773.jpg?c=2"
           text={user}
         />
         <br></br>
+        <QuestStart setGameId={setGameId} userKey={userKey} />
+        <br></br>
         <div>
           <h3 className="previous-game-header">Previous Game End:</h3>
-          <OldGameBoard width={200}/>    
+          <OldGameBoard width={200} />
         </div>
       </section>
     </>
