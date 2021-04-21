3f3175e0b75318e18dfbd07023b97487b22a153d
Elsa Fluss <elsafluss@gmail.com>
2021-04-20 22:27:08 -0600

    Trying to define URL_ROOT

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,68 +3,57 @@
 /// <reference types="cypress" />
 
 describe("Sign Up and Log In", () => {
-  it(
-    "should display the login screen",
-    {
-      env: {
-        API_ROOT: "http://localhost:3001",
-        API_WS_ROOT: "ws://localhost:3001/cable",
-        URL_ROOT: "http://localhost:3000",
-      },
-    },
+  it.only("should display the login screen", () => {
+    cy.visit(Cypress.env("URL_ROOT"))
+    cy.get(".splash").should("exist").should("have.descendants", "section")
 
-    () => {
-      cy.visit(Cypress.env('URL_ROOT'))
-      cy.get(".splash").should("exist").should("have.descendants", "section")
+    cy.get(".login-modal").should("exist").should("have.descendants", "h1")
 
-      cy.get(".login-modal").should("exist").should("have.descendants", "h1")
+    cy.get(".title").should("exist").should("have.text", "ChessPedition")
 
-      cy.get(".title").should("exist").should("have.text", "ChessPedition")
+    cy.get(".form-wrapper")
+      .should("exist")
+      .should("have.text", "Username:Password:Enter")
+      .should("have.descendants", "form")
 
-      cy.get(".form-wrapper")
-        .should("exist")
-        .should("have.text", "Username:Password:Enter")
-        .should("have.descendants", "form")
+    cy.get(".form").should("exist").should("have.descendants", "label")
 
-      cy.get(".form").should("exist").should("have.descendants", "label")
+    cy.get(".label")
+      .eq(0)
+      .should("exist")
+      .should("have.text", "Username:")
+      .should("have.descendants", "br")
+      .should("have.descendants", "input")
 
-      cy.get(".label")
-        .eq(0)
-        .should("exist")
-        .should("have.text", "Username:")
-        .should("have.descendants", "br")
-        .should("have.descendants", "input")
+    cy.get("input")
+      .eq(0)
+      .should("exist")
+      .should("have.attr", "type", "text")
+      .should("have.attr", "class", "input")
+      .should("have.attr", "name", "username")
 
-      cy.get("input")
-        .eq(0)
-        .should("exist")
-        .should("have.attr", "type", "text")
-        .should("have.attr", "class", "input")
-        .should("have.attr", "name", "username")
+    cy.get(".label")
+      .eq(1)
+      .should("exist")
+      .should("have.text", "Password:")
+      .should("have.descendants", "br")
+      .should("have.descendants", "input")
 
-      cy.get(".label")
-        .eq(1)
-        .should("exist")
-        .should("have.text", "Password:")
-        .should("have.descendants", "br")
-        .should("have.descendants", "input")
+    cy.get("input")
+      .eq(1)
+      .should("exist")
+      .should("have.attr", "type", "password")
+      .should("have.attr", "class", "input")
+      .should("have.attr", "name", "password")
 
-      cy.get("input")
-        .eq(1)
-        .should("exist")
-        .should("have.attr", "type", "password")
-        .should("have.attr", "class", "input")
-        .should("have.attr", "name", "password")
+    cy.get(".log-in-wrapper")
+      .should("exist")
+      .should("have.descendants", "button")
 
-      cy.get(".log-in-wrapper")
-        .should("exist")
-        .should("have.descendants", "button")
+    cy.get(".log-in").should("exist").should("have.text", "Enter")
 
-      cy.get(".log-in").should("exist").should("have.text", "Enter")
-
-      cy.get(".signup-button").should("exist").should("have.text", "Sign Up")
-    }
-  )
+    cy.get(".signup-button").should("exist").should("have.text", "Sign Up")
+  })
 
   it(
     "should display the sign up screen",
@@ -164,7 +153,7 @@ describe("Sign Up and Log In", () => {
       cy.get("input").eq(2).type("testpassword")
       cy.get("input").eq(2).should("have.value", "testpassword")
 
-      const API_ROOT = Cypress.env('API_ROOT')
+      const API_ROOT = Cypress.env("API_ROOT")
       cy.intercept(
         {
           method: "POST",
@@ -177,7 +166,7 @@ describe("Sign Up and Log In", () => {
       )
 
       cy.get(".log-in").click({ force: true })
-      
+
       cy.location().should((loc) => {
         expect(loc.host).to.eq("localhost:3000")
         expect(loc.href).to.eq(`${URL_ROOT}/dashboard`)
