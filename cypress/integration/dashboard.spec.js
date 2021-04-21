2bf7c88ae96dc67d550422db1f1a9b84ca950dc5
Elsa Fluss <elsafluss@gmail.com>
2021-04-20 22:39:35 -0600

    Successfully ? call cypress env variables?!

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -3,246 +3,217 @@
 /// <reference types="cypress" />
 
 describe("Show dashboard", () => {
-  it.skip(
-    "should display the dashboard",
-    {
-      env: {
-        API_ROOT: "http://localhost:3001",
-        API_WS_ROOT: "ws://localhost:3001/cable",
-        URL_ROOT: "http://localhost:3000",
+  it("should display the dashboard", () => {
+    const API_ROOT = Cypress.env("API_ROOT")
+    cy.visit(Cypress.env("URL_ROOT"))
+    cy.get(".signup-button").click()
+
+    cy.get("input").eq(0).type("test user name")
+    cy.get("input").eq(0).should("have.value", "test user name")
+
+    cy.get("input").eq(1).type("testpassword")
+    cy.get("input").eq(1).should("have.value", "testpassword")
+    cy.get("input").eq(2).type("testpassword")
+    cy.get("input").eq(2).should("have.value", "testpassword")
+
+    cy.intercept(
+      {
+        method: "POST",
+        url: `${API_ROOT}/api/v1/users`,
       },
-    },
+      {
+        status: 200,
+        fixture: "login.json",
+      }
+    )
 
-    () => {
-      cy.visit(Cypress.env("URL_ROOT"))
-      cy.get(".signup-button").click()
+    cy.get(".log-in").click({ force: true })
 
-      cy.get("input").eq(0).type("test user name")
-      cy.get("input").eq(0).should("have.value", "test user name")
+    cy.get(".dashboard-header").should("exist").should("have.descendants", "h1")
+    cy.get(".dashboard-header")
+      .should("exist")
+      .should("have.descendants", "button")
 
-      cy.get("input").eq(1).type("testpassword")
-      cy.get("input").eq(1).should("have.value", "testpassword")
-      cy.get("input").eq(2).type("testpassword")
-      cy.get("input").eq(2).should("have.value", "testpassword")
+    cy.get("h1").should("have.text", "ChessPedition")
+    cy.get("button").eq(0).should("have.text", "Sign Out")
 
-      cy.intercept(
-        {
-          method: "POST",
-          url: `${API_ROOT}/api/v1/users`,
-        },
-        {
-          status: 200,
-          fixture: "login.json",
-        }
+    cy.get(".container")
+      .should("exist")
+      .should("have.descendants", "div.greeting")
+      .should("have.descendants", "p")
+      .should("have.descendants", "button")
+      .should("have.descendants", "section.quest-start")
+      .should("have.descendants", "div.rules-container")
+      .should("have.descendants", "section")
+
+    cy.get(".thumbnail-text")
+      .should("exist")
+      .should("have.text", "test user name")
+
+    cy.get(".greeting")
+      .should("exist")
+      .should("have.text", "Welcome, test user name")
+
+    cy.get(".quest-start")
+      .should("exist")
+      .should("have.attr", "id", "quest-start")
+      .should("have.descendants", "button")
+    cy.get(".start-button")
+      .should("exist")
+      .should("have.text", "Start A New ChessPedition")
+
+    cy.get(".rules-container")
+      .should("exist")
+      .should("have.descendants", "h3.rules-text")
+
+    cy.get(".rules-text")
+      .eq(0)
+      .should(
+        "have.text",
+        "♟Click Start A New ChessPedition and send the game URL to your opponent."
+      )
+      .should("have.descendants", "span.chess-piece")
+
+    cy.get(".rules-text")
+      .eq(1)
+      .should(
+        "have.text",
+        "♟If you win the game, you carry only your leftover pieces to the next game!"
       )
 
-      cy.get(".log-in").click({ force: true })
+    cy.get(".rules-text")
+      .eq(2)
+      .should(
+        "have.text",
+        "♟If you lose the game, you get a full set of pieces."
+      )
 
-      cy.get(".dashboard-header")
-        .should("exist")
-        .should("have.descendants", "h1")
-      cy.get(".dashboard-header")
-        .should("exist")
-        .should("have.descendants", "button")
+    cy.get(".rules-text")
+      .eq(3)
+      .should(
+        "have.text",
+        "♟Keep playing against each other to find the true ChessPedition champion!"
+      )
 
-      cy.get("h1").should("have.text", "ChessPedition")
-      cy.get("button").eq(0).should("have.text", "Sign Out")
+    cy.get(".previous-game-header")
+      .should("exist")
+      .should("have.text", "Play a game and its end board will show here.")
+  })
 
-      cy.get(".container")
-        .should("exist")
-        .should("have.descendants", "div.greeting")
-        .should("have.descendants", "p")
-        .should("have.descendants", "button")
-        .should("have.descendants", "section.quest-start")
-        .should("have.descendants", "div.rules-container")
-        .should("have.descendants", "section")
+  it("should sign the user out", () => {
+    const URL_ROOT = Cypress.env("URL_ROOT")
+    const API_ROOT = Cypress.env("API_ROOT")
+    cy.visit(Cypress.env("URL_ROOT"))
+    cy.get(".signup-button").click()
 
-      cy.get(".thumbnail-text")
-        .should("exist")
-        .should("have.text", "test user name")
+    cy.get("input").eq(0).type("test user name")
+    cy.get("input").eq(0).should("have.value", "test user name")
 
-      cy.get(".greeting")
-        .should("exist")
-        .should("have.text", "Welcome, test user name")
+    cy.get("input").eq(1).type("testpassword")
+    cy.get("input").eq(1).should("have.value", "testpassword")
+    cy.get("input").eq(2).type("testpassword")
+    cy.get("input").eq(2).should("have.value", "testpassword")
 
-      cy.get(".quest-start")
-        .should("exist")
-        .should("have.attr", "id", "quest-start")
-        .should("have.descendants", "button")
-      cy.get(".start-button")
-        .should("exist")
-        .should("have.text", "Start A New ChessPedition")
-
-      cy.get(".rules-container")
-        .should("exist")
-        .should("have.descendants", "h3.rules-text")
-
-      cy.get(".rules-text")
-        .eq(0)
-        .should(
-          "have.text",
-          "♟Click Start A New ChessPedition and send the game URL to your opponent."
-        )
-        .should("have.descendants", "span.chess-piece")
-
-      cy.get(".rules-text")
-        .eq(1)
-        .should(
-          "have.text",
-          "♟If you win the game, you carry only your leftover pieces to the next game!"
-        )
-
-      cy.get(".rules-text")
-        .eq(2)
-        .should(
-          "have.text",
-          "♟If you lose the game, you get a full set of pieces."
-        )
-
-      cy.get(".rules-text")
-        .eq(3)
-        .should(
-          "have.text",
-          "♟Keep playing against each other to find the true ChessPedition champion!"
-        )
-
-      cy.get(".previous-game-header")
-        .should("exist")
-        .should("have.text", "Play a game and its end board will show here.")
-    }
-  )
-
-  it.skip(
-    "should sign the user out",
-    {
-      env: {
-        API_ROOT: "http://localhost:3001",
-        API_WS_ROOT: "ws://localhost:3001/cable",
-        URL_ROOT: "http://localhost:3000",
+    cy.intercept(
+      {
+        method: "POST",
+        url: `${API_ROOT}/api/v1/users`,
       },
-    },
-    () => {
-      cy.visit(Cypress.env("URL_ROOT"))
-      cy.get(".signup-button").click()
+      {
+        status: 200,
+        fixture: "login.json",
+      }
+    )
 
-      cy.get("input").eq(0).type("test user name")
-      cy.get("input").eq(0).should("have.value", "test user name")
+    cy.get(".log-in").click({ force: true })
 
-      cy.get("input").eq(1).type("testpassword")
-      cy.get("input").eq(1).should("have.value", "testpassword")
-      cy.get("input").eq(2).type("testpassword")
-      cy.get("input").eq(2).should("have.value", "testpassword")
+    cy.get(".thumbnail-text").should("have.text", "test user name")
 
-      cy.intercept(
-        {
-          method: "POST",
-          url: `${API_ROOT}/api/v1/users`,
-        },
-        {
-          status: 200,
-          fixture: "login.json",
-        }
-      )
+    cy.wait(2000)
 
-      cy.get(".log-in").click({ force: true })
+    cy.get("button").eq(0).click()
 
-      cy.get(".thumbnail-text").should("have.text", "test user name")
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq(URL_ROOT)
+    })
+  })
 
-      cy.wait(2000)
+  it("should start a new game and return to the dashboard", () => {
+    const URL_ROOT = Cypress.env("URL_ROOT")
+    const API_ROOT = Cypress.env("API_ROOT")
 
-      cy.get("button").eq(0).click()
+    cy.visit(Cypress.env("URL_ROOT"))
+    cy.get(".signup-button").click()
 
-      cy.location().should((loc) => {
-        expect(loc.href).to.eq(URL_ROOT)
-      })
-    }
-  )
+    cy.get("input").eq(0).type("test user name")
+    cy.get("input").eq(0).should("have.value", "test user name")
 
-  it.skip(
-    "should start a new game and return to the dashboard",
-    {
-      env: {
-        API_ROOT: "http://localhost:3001",
-        API_WS_ROOT: "ws://localhost:3001/cable",
-        URL_ROOT: "http://localhost:3000",
+    cy.get("input").eq(1).type("testpassword")
+    cy.get("input").eq(1).should("have.value", "testpassword")
+    cy.get("input").eq(2).type("testpassword")
+    cy.get("input").eq(2).should("have.value", "testpassword")
+
+    cy.intercept(
+      {
+        method: "POST",
+        url: `${API_ROOT}/api/v1/users`,
       },
-    },
-    () => {
-      cy.visit(Cypress.env("URL_ROOT"))
-      cy.get(".signup-button").click()
+      {
+        status: 200,
+        fixture: "login.json",
+      }
+    )
 
-      cy.get("input").eq(0).type("test user name")
-      cy.get("input").eq(0).should("have.value", "test user name")
+    cy.get(".log-in").click({ force: true })
 
-      cy.get("input").eq(1).type("testpassword")
-      cy.get("input").eq(1).should("have.value", "testpassword")
-      cy.get("input").eq(2).type("testpassword")
-      cy.get("input").eq(2).should("have.value", "testpassword")
+    cy.intercept(
+      {
+        method: "POST",
+        url: `${API_ROOT}/api/v1/friendly_games`,
+      },
+      {
+        status: 200,
+        fixture: "start-game.json",
+      }
+    )
+    cy.get("button").eq(1).click()
 
-      cy.intercept(
-        {
-          method: "POST",
-          url: `${API_ROOT}/api/v1/users`,
-        },
-        {
-          status: 200,
-          fixture: "login.json",
-        }
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq(`${URL_ROOT}/game/test`)
+    })
+
+    cy.get(".dashboard-header").should("exist").should("have.descendants", "h1")
+    cy.get(".dashboard-header")
+      .should("exist")
+      .should("have.descendants", "button")
+
+    cy.get(".new-game-link-container")
+      .should("exist")
+      .should("have.descendants", "p.new-game-link-text")
+
+    cy.get(".new-game-link-text")
+      .should("exist")
+      .should(
+        "have.text",
+        "Send this link to a friend to start playing!http://localhost:3000/game/testThe game board will appear when the second player joins the room."
       )
+      .should("have.descendants", "span.new-game-link")
 
-      cy.get(".log-in").click({ force: true })
+    cy.get(".new-game-link")
+      .should("exist")
+      .should("have.text", `${URL_ROOT}/game/test`)
 
-      cy.intercept(
-        {
-          method: "POST",
-          url: `${API_ROOT}/api/v1/friendly_games`,
-        },
-        {
-          status: 200,
-          fixture: "start-game.json",
-        }
-      )
-      cy.get("button").eq(1).click()
+    cy.get(".game-screen-lower-third")
+      .should("exist")
+      .should("have.descendants", "button.leave-game")
 
-      cy.location().should((loc) => {
-        expect(loc.href).to.eq(`${URL_ROOT}/game/test`)
-      })
+    cy.get(".leave-game")
+      .should("exist")
+      .should("have.text", "Back to Dashboard")
+      .click()
 
-      cy.get(".dashboard-header")
-        .should("exist")
-        .should("have.descendants", "h1")
-      cy.get(".dashboard-header")
-        .should("exist")
-        .should("have.descendants", "button")
-
-      cy.get(".new-game-link-container")
-        .should("exist")
-        .should("have.descendants", "p.new-game-link-text")
-
-      cy.get(".new-game-link-text")
-        .should("exist")
-        .should(
-          "have.text",
-          "Send this link to a friend to start playing!http://localhost:3000/game/testThe game board will appear when the second player joins the room."
-        )
-        .should("have.descendants", "span.new-game-link")
-
-      cy.get(".new-game-link")
-        .should("exist")
-        .should("have.text", `${URL_ROOT}/game/test`)
-
-      cy.get(".game-screen-lower-third")
-        .should("exist")
-        .should("have.descendants", "button.leave-game")
-
-      cy.get(".leave-game")
-        .should("exist")
-        .should("have.text", "Back to Dashboard")
-        .click()
-
-      cy.location().should((loc) => {
-        expect(loc.href).to.eq(`${URL_ROOT}/dashboard`)
-      })
-    }
-  )
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq(`${URL_ROOT}/dashboard`)
+    })
+  })
 })
--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,7 +3,7 @@
 /// <reference types="cypress" />
 
 describe("Sign Up and Log In", () => {
-  it.only("should display the login screen", () => {
+  it("should display the login screen", () => {
     cy.visit(Cypress.env("URL_ROOT"))
     cy.get(".splash").should("exist").should("have.descendants", "section")
 
@@ -55,122 +55,103 @@ describe("Sign Up and Log In", () => {
     cy.get(".signup-button").should("exist").should("have.text", "Sign Up")
   })
 
-  it(
-    "should display the sign up screen",
-    {
-      env: {
-        API_ROOT: "http://localhost:3001",
-        API_WS_ROOT: "ws://localhost:3001/cable",
-        URL_ROOT: "http://localhost:3000",
+  it("should display the sign up screen", () => {
+    cy.visit(Cypress.env("URL_ROOT"))
+    cy.get(".signup-button").click()
+
+    cy.get(".splash").should("exist").should("have.descendants", "section")
+
+    cy.get(".login-modal").should("exist").should("have.descendants", "h1")
+
+    cy.get(".title").should("exist").should("have.text", "ChessPedition")
+
+    cy.get(".form-wrapper")
+      .should("exist")
+      .should("have.text", "Username:Password:Confirm Password:Enter")
+      .should("have.descendants", "form")
+
+    cy.get(".form").should("exist").should("have.descendants", "label")
+
+    cy.get(".label")
+      .eq(0)
+      .should("exist")
+      .should("have.text", "Username:")
+      .should("have.descendants", "br")
+      .should("have.descendants", "input")
+
+    cy.get("input")
+      .eq(0)
+      .should("exist")
+      .should("have.attr", "type", "text")
+      .should("have.attr", "class", "input")
+      .should("have.attr", "name", "username")
+
+    cy.get(".label")
+      .eq(1)
+      .should("exist")
+      .should("have.text", "Password:")
+      .should("have.descendants", "br")
+      .should("have.descendants", "input")
+
+    cy.get("input")
+      .eq(1)
+      .should("exist")
+      .should("have.attr", "type", "password")
+      .should("have.attr", "class", "input")
+      .should("have.attr", "name", "password")
+    cy.get(".label")
+      .eq(1)
+      .should("exist")
+      .should("have.text", "Password:")
+      .should("have.descendants", "br")
+      .should("have.descendants", "input")
+
+    cy.get("input")
+      .eq(2)
+      .should("exist")
+      .should("have.attr", "type", "password")
+      .should("have.attr", "class", "input")
+      .should("have.attr", "name", "confirmPassword")
+
+    cy.get(".log-in-wrapper")
+      .should("exist")
+      .should("have.descendants", "button")
+
+    cy.get(".log-in").should("exist").should("have.text", "Enter")
+
+    cy.get(".signup-button").should("exist").should("have.text", "Log In")
+  })
+
+  it("should sign up the user", () => {
+    const URL_ROOT = Cypress.env("URL_ROOT")
+    const API_ROOT = Cypress.env("API_ROOT")
+    cy.visit(Cypress.env("URL_ROOT"))
+    cy.get(".signup-button").click()
+
+    cy.get("input").eq(0).type("test user name")
+    cy.get("input").eq(0).should("have.value", "test user name")
+
+    cy.get("input").eq(1).type("testpassword")
+    cy.get("input").eq(1).should("have.value", "testpassword")
+    cy.get("input").eq(2).type("testpassword")
+    cy.get("input").eq(2).should("have.value", "testpassword")
+
+    cy.intercept(
+      {
+        method: "POST",
+        url: `${API_ROOT}/api/v1/users`,
       },
-    },
-    () => {
-      cy.visit(Cypress.env("URL_ROOT"))
-      cy.get(".signup-button").click()
+      {
+        status: 200,
+        fixture: "login.json",
+      }
+    )
 
-      cy.get(".splash").should("exist").should("have.descendants", "section")
+    cy.get(".log-in").click({ force: true })
 
-      cy.get(".login-modal").should("exist").should("have.descendants", "h1")
-
-      cy.get(".title").should("exist").should("have.text", "ChessPedition")
-
-      cy.get(".form-wrapper")
-        .should("exist")
-        .should("have.text", "Username:Password:Confirm Password:Enter")
-        .should("have.descendants", "form")
-
-      cy.get(".form").should("exist").should("have.descendants", "label")
-
-      cy.get(".label")
-        .eq(0)
-        .should("exist")
-        .should("have.text", "Username:")
-        .should("have.descendants", "br")
-        .should("have.descendants", "input")
-
-      cy.get("input")
-        .eq(0)
-        .should("exist")
-        .should("have.attr", "type", "text")
-        .should("have.attr", "class", "input")
-        .should("have.attr", "name", "username")
-
-      cy.get(".label")
-        .eq(1)
-        .should("exist")
-        .should("have.text", "Password:")
-        .should("have.descendants", "br")
-        .should("have.descendants", "input")
-
-      cy.get("input")
-        .eq(1)
-        .should("exist")
-        .should("have.attr", "type", "password")
-        .should("have.attr", "class", "input")
-        .should("have.attr", "name", "password")
-      cy.get(".label")
-        .eq(1)
-        .should("exist")
-        .should("have.text", "Password:")
-        .should("have.descendants", "br")
-        .should("have.descendants", "input")
-
-      cy.get("input")
-        .eq(2)
-        .should("exist")
-        .should("have.attr", "type", "password")
-        .should("have.attr", "class", "input")
-        .should("have.attr", "name", "confirmPassword")
-
-      cy.get(".log-in-wrapper")
-        .should("exist")
-        .should("have.descendants", "button")
-
-      cy.get(".log-in").should("exist").should("have.text", "Enter")
-
-      cy.get(".signup-button").should("exist").should("have.text", "Log In")
-    }
-  )
-
-  it(
-    "should sign up the user",
-    {
-      env: {
-        API_ROOT: "http://localhost:3001",
-        API_WS_ROOT: "ws://localhost:3001/cable",
-        URL_ROOT: "http://localhost:3000",
-      },
-    },
-    () => {
-      cy.visit(Cypress.env("URL_ROOT"))
-      cy.get(".signup-button").click()
-
-      cy.get("input").eq(0).type("test user name")
-      cy.get("input").eq(0).should("have.value", "test user name")
-
-      cy.get("input").eq(1).type("testpassword")
-      cy.get("input").eq(1).should("have.value", "testpassword")
-      cy.get("input").eq(2).type("testpassword")
-      cy.get("input").eq(2).should("have.value", "testpassword")
-
-      const API_ROOT = Cypress.env("API_ROOT")
-      cy.intercept(
-        {
-          method: "POST",
-          url: `${API_ROOT}/api/v1/users`,
-        },
-        {
-          status: 200,
-          fixture: "login.json",
-        }
-      )
-
-      cy.get(".log-in").click({ force: true })
-
-      cy.location().should((loc) => {
-        expect(loc.host).to.eq("localhost:3000")
-        expect(loc.href).to.eq(`${URL_ROOT}/dashboard`)
-      })
-    }
-  )
+    cy.location().should((loc) => {
+      expect(loc.host).to.eq("localhost:3000")
+      expect(loc.href).to.eq(`${URL_ROOT}/dashboard`)
+    })
+  })
 })
